095b5976d8936bfd8fef707433e02675518260ab
svaleria <svaleria.gonzales@gmail.com>
2025-03-20 18:24:44 -0400

    initial springboot project

---
+++ src/main/java/com/iws_manager/iws_manager_api/IwsManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.iws_manager.iws_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwsManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwsManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iws_manager/iws_manager_api/IwsManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iws_manager.iws_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwsManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
