455a8ed73354b55e83d0cf97fa06f8cf55be563e
RodrigoGutierrez <rodrigo.gutierrez.acc@gmail.com>
2025-05-04 22:27:25 -0400

    Added unit tests for salutation

---
+++ src/test/java/com/iws_manager/iws_manager_api/models/SalutationTest.java
@@ -0,0 +1,50 @@
+package com.iws_manager.iws_manager_api.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SalutationTest {
+
+    private Salutation salutation;
+
+    @Test
+    void testSalutationModel() {
+        String uuid = UUID.randomUUID().toString();
+        String salutationName = "Frau";
+
+        salutation = new Salutation();
+        salutation.setUuid(uuid);
+        salutation.setSalutation(salutationName);
+
+        assertThat(salutation.getUuid()).isEqualTo(uuid);
+        assertThat(salutation.getSalutation()).isEqualTo(salutationName);
+    }
+
+    @Test
+    void testTitleConstructor() {
+        String uuid = UUID.randomUUID().toString();
+        String salutationName = "Frau";
+
+        salutation = new Salutation(1, uuid, salutationName);
+
+        // Assert
+        assertThat(salutation.getId()).isEqualTo(1);
+        assertThat(salutation.getUuid()).isEqualTo(uuid);
+        assertThat(salutation.getSalutation()).isEqualTo(salutationName);
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        // Arrange
+        String uuid = UUID.randomUUID().toString();
+        Salutation salutation1 = new Salutation(1, uuid, "Herr");
+        Salutation salutation2 = new Salutation(1, uuid, "Herr");
+
+        // Assert
+        assertThat(salutation1).isEqualTo(salutation2);
+        assertThat(salutation2.hashCode()).isEqualTo(salutation2.hashCode());
+    }
+}
