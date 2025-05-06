42770dedea89d253d075df4835838148a2598778
svaleria <svaleria.gonzales@gmail.com>
2025-05-06 18:54:06 -0400

    security and pipeline fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,102 +4,84 @@ object Versions {
 }
 
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
-	//id("org.sonarqube") version "4.4.1.3373"
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "6.1.0.5360"
-    jacoco
-	`java-library`
+    `java-library`
 }
 
 group = "com.iws-manager"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// Spring Boot
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	
-	// Lombok
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
-	// Developer tools
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	
-	// Database
-	runtimeOnly("org.postgresql:postgresql")
+    // Developer tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    
+    // Database
+    runtimeOnly("org.postgresql:postgresql")
 
-	// Testing
-	// testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-	// testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.junit.jupiter:junit-jupiter:${Versions.JUNIT}")
-	testImplementation("org.mockito:mockito-core:${Versions.MOCKITO}")
-	testImplementation("org.mockito:mockito-junit-jupiter:${Versions.MOCKITO}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${Versions.JUNIT}")
+    testImplementation("org.mockito:mockito-core:${Versions.MOCKITO}")
+    testImplementation("org.mockito:mockito-junit-jupiter:${Versions.MOCKITO}")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 
-	reports {
+    reports {
         html.required.set(true)
         junitXml.required.set(true) 
     }
 
-    configure<JacocoTaskExtension> {
-        isIncludeNoLocationClasses = true
-        excludes = listOf("jdk.internal.*")
-    }
-
-	testLogging {
+    testLogging {
         events("passed", "failed", "skipped")
         showStandardStreams = false
     }
-	outputs.dir("build/test-results")
-    finalizedBy(tasks.jacocoTestReport)
+    outputs.dir("build/test-results")
 }
 
 sonar {
     properties {
-         // SonarCloud Setup
+        // SonarCloud Setup
         property("sonar.projectKey", "Piramide-Informatik_iws-manager-api")
         property("sonar.host.url", "https://sonarcloud.io")
-        //property("sonar.organization", "piramide-informatik") // Asegúrate de que sea correcto
-        //property("sonar.login", "cf07764d46f4e04f91dfd286da7fb05babb60519")
 
         // Routing Analysis setup
         property("sonar.java.binaries", "build/classes/java/main")
         property("sonar.java.test.binaries", "build/classes/java/test")
 
-		property("sonar.junit.reportPaths", "build/test-results/test")
-        property("sonar.coverage.jacoco.xmlReportPaths", "")
-        property("sonar.jacoco.reportPaths", "") 
-        property("sonar.java.coveragePlugin", "none")
-        // property("sonar.jacoco.reportPaths", "build/jacoco/test.exec")
-        // property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
-        // property("sonar.junit.reportPaths", listOf("build/test-results/test"))
-        // property("sonar.jacoco.reportPaths", listOf("build/jacoco/test.exec"))
-        // property("sonar.coverage.jacoco.xmlReportPaths", listOf("build/reports/jacoco/test/jacocoTestReport.xml"))
+        property("sonar.junit.reportPaths", "build/test-results/test")
+//        property("sonar.java.coveragePlugin", "junit")
+        property("sonar.testExecutionReportPaths", "build/test-results/test")
 
         property("sonar.sources", "src/main/java")
         property("sonar.tests", "src/test/java")
@@ -107,42 +89,15 @@ sonar {
         // Extra configurations
         property("sonar.java.source", "21")
         property("sonar.sourceEncoding", "UTF-8")
-        property("sonar.qualitygate.wait", "true")
+//        property("sonar.qualitygate.wait", "true")
         property("sonar.scm.provider", "git")
         
         // Delete duplicity
         property("sonar.gradle.skipCompile", "true")
 
-        // Analysis exclussion
+        // Analysis exclusion
         property("sonar.exclusions", "**/config/**,**/exception/**,**/*Application*")
-        // property("sonar.exclusions", "**/config/**,**/exception/**,**/models/**,**/*Application*")
-        // property("sonar.exclusions", listOf("**/config/**", "**/exception/**", "**/models/**", "**/*Application*"))
     }
-}
-
-tasks.test {
-    //finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.jacocoTestReport {
-    enabled = false 
-    // dependsOn(tasks.test)
-    // reports {
-    //     xml.required.set(true) 
-    //     html.required.set(true)
-    // }
-	// classDirectories.setFrom(files(classDirectories.files.map {
-    //     fileTree(it).apply {
-    //         include("**/*.class")
-    //         exclude(
-    //             "**/config/**",
-    //             "**/exception/**",
-    //             "**/*Application*",
-    //             "**/*\$*"
-    //         )
-    //     }
-    // }))
-	// executionData.setFrom(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
 }
 
 tasks.register("validateDependencies") {
