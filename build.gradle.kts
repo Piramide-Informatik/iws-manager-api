4fae3f69f4620c6e7147fb1c43b7c93f94837c6f
svaleria <svaleria.gonzales@gmail.com>
2025-05-06 12:34:35 -0400

    exclude jacoco for sonarcloud reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,11 @@ tasks.withType<Test> {
         junitXml.required.set(true) 
     }
 
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+
 	testLogging {
         events("passed", "failed", "skipped")
         showStandardStreams = false
@@ -79,14 +84,19 @@ sonar {
          // SonarCloud Setup
         property("sonar.projectKey", "Piramide-Informatik_iws-manager-api")
         property("sonar.host.url", "https://sonarcloud.io")
+        //property("sonar.organization", "piramide-informatik") // Asegúrate de que sea correcto
+        //property("sonar.login", "cf07764d46f4e04f91dfd286da7fb05babb60519")
 
         // Routing Analysis setup
         property("sonar.java.binaries", "build/classes/java/main")
         property("sonar.java.test.binaries", "build/classes/java/test")
 
 		property("sonar.junit.reportPaths", "build/test-results/test")
-        property("sonar.jacoco.reportPaths", "build/jacoco/test.exec")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "")
+        property("sonar.jacoco.reportPaths", "") 
+        property("sonar.java.coveragePlugin", "none")
+        // property("sonar.jacoco.reportPaths", "build/jacoco/test.exec")
+        // property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
         // property("sonar.junit.reportPaths", listOf("build/test-results/test"))
         // property("sonar.jacoco.reportPaths", listOf("build/jacoco/test.exec"))
         // property("sonar.coverage.jacoco.xmlReportPaths", listOf("build/reports/jacoco/test/jacocoTestReport.xml"))
@@ -104,33 +114,35 @@ sonar {
         property("sonar.gradle.skipCompile", "true")
 
         // Analysis exclussion
-        property("sonar.exclusions", "**/config/**,**/exception/**,**/models/**,**/*Application*")
+        property("sonar.exclusions", "**/config/**,**/exception/**,**/*Application*")
+        // property("sonar.exclusions", "**/config/**,**/exception/**,**/models/**,**/*Application*")
         // property("sonar.exclusions", listOf("**/config/**", "**/exception/**", "**/models/**", "**/*Application*"))
     }
 }
 
 tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
+    //finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true) 
-        html.required.set(true)
-    }
-	classDirectories.setFrom(files(classDirectories.files.map {
-        fileTree(it).apply {
-            include("**/*.class")
-            exclude(
-                "**/config/**",
-                "**/exception/**",
-                "**/models/**",
-                "**/*Application*"
-            )
-        }
-    }))
-	executionData.setFrom(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
+    enabled = false 
+    // dependsOn(tasks.test)
+    // reports {
+    //     xml.required.set(true) 
+    //     html.required.set(true)
+    // }
+	// classDirectories.setFrom(files(classDirectories.files.map {
+    //     fileTree(it).apply {
+    //         include("**/*.class")
+    //         exclude(
+    //             "**/config/**",
+    //             "**/exception/**",
+    //             "**/*Application*",
+    //             "**/*\$*"
+    //         )
+    //     }
+    // }))
+	// executionData.setFrom(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
 }
 
 tasks.register("validateDependencies") {
