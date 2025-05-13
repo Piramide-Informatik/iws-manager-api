271c1877fa02c07ec21360cac44b0d33c23c0aa8
svaleria <svaleria.gonzales@gmail.com>
2025-05-13 11:36:38 -0400

    state repository fixed

--- src/main/java/com/iws_manager/iws_manager_api/repositories/StateRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/StateRepository.java
@@ -2,11 +2,9 @@ package com.iws_manager.iws_manager_api.repositories;
 
 import com.iws_manager.iws_manager_api.models.State;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface StateRepository extends JpaRepository<State, Integer> {
-    Optional<State> findByUuid(String uuid);
+public interface StateRepository extends JpaRepository<State, Long> {
 }
