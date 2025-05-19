ab37b17c3f3f08cf1b8735709afaf19cd6c0a40c
svaleria <svaleria.gonzales@gmail.com>
2025-05-19 11:28:58 -0400

    jpa auditing enabled

--- src/main/java/com/iws_manager/iws_manager_api/IwsManagerApiApplication.java
+++ src/main/java/com/iws_manager/iws_manager_api/IwsManagerApiApplication.java
@@ -3,8 +3,10 @@ package com.iws_manager.iws_manager_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication //(exclude = DataSourceAutoConfiguration.class)
+@EnableJpaAuditing
 @EntityScan(basePackages = "com.iws_manager.iws_manager_api.models")
 public class IwsManagerApiApplication {
 
