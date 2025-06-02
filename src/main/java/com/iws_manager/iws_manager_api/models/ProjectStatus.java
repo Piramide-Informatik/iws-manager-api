7d62bbfd309dec1f8138a9741754a7a8b69f1764
svaleria <svaleria.gonzales@gmail.com>
2025-06-02 11:10:11 -0400

    project status model created

---
+++ src/main/java/com/iws_manager/iws_manager_api/models/ProjectStatus.java
@@ -0,0 +1,28 @@
+package com.iws_manager.iws_manager_api.models;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.iws_manager.iws_manager_api.models.base.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Entity representing a German federal project status (Bundesland).
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "project-status")
+public class ProjectStatus extends BaseEntity{
+
+    /**
+     * Official name of the project-status (e.g., "Bavaria", "Berlin", "Hamburg").
+     */
+    @Column(name = "name", nullable = false)
+    private String name;
+}
