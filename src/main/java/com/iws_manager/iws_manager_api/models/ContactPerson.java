3ed34088b8e427d01012a86c9054e377233e2bc3
svaleria <svaleria.gonzales@gmail.com>
2025-06-02 20:48:59 -0400

    contact person model according to db

--- src/main/java/com/iws_manager/iws_manager_api/models/ContactPerson.java
+++ src/main/java/com/iws_manager/iws_manager_api/models/ContactPerson.java
@@ -1,10 +1,21 @@
 package com.iws_manager.iws_manager_api.models;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.iws_manager.iws_manager_api.models.base.BaseEntity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Entity representing a contact person associated with a customer.
@@ -16,51 +27,51 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "contactPerson")
+@Table(name = "contactperson")
 public class ContactPerson extends BaseEntity{
 
     /**
      * Customer to whom the contact person is linked.
      */
     @ManyToOne
-    @JoinColumn(name = "customer_id")
+    @JoinColumn(name = "customerid", referencedColumnName = "id", nullable = false)
     private Customer customer;
-
-    /**
-     * Title of the contact person (e.g., Mr., Mrs.).
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "title_id", nullable = false)
-    private Title title;
-
-    /**
-     * Salutation of the contact person (e.g., Dr., Eng.).
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "salutation_id", nullable = false)
-    private Salutation salutation;
 
     /**
      * First name of the contact person.
      */
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "firstname", nullable = true, length = 255)
     private String firstName;
-
-    /**
-     * Last name of the contact person.
-     */
-    @Column(name = "lastName", nullable = false)
-    private String lastName;
 
     /**
      * Indicates whether the contact person is responsible for invoicing.
      */
-    @Column(name = "forInvoincing", nullable = false)
+    @Column(name = "forinvoincing", nullable = true)
     private Integer forInvoincing;
 
     /**
      * Job position or role of the contact person.
      */
-    @Column(name = "function", nullable = false)
+    @Column(name = "`function`", nullable = true, length = 255)
     private String function;
+
+    /**
+     * Last name of the contact person.
+     */
+    @Column(name = "lastname", nullable = true, length = 255)
+    private String lastName;
+
+    /**
+     * Salutation of the contact person (e.g., Dr., Eng.).
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "salutationid",  referencedColumnName = "id", nullable = false)
+    private Salutation salutation;
+
+    /**
+     * Title of the contact person (e.g., Mr., Mrs.).
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "titleid",  referencedColumnName = "id", nullable = false)
+    private Title title;
 }
