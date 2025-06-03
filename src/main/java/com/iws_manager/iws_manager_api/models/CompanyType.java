df87103326f03c8ea596b51b5f0357ffe3554006
svaleria <svaleria.gonzales@gmail.com>
2025-06-02 20:11:10 -0400

    company type model with db

--- src/main/java/com/iws_manager/iws_manager_api/models/CompanyType.java
+++ src/main/java/com/iws_manager/iws_manager_api/models/CompanyType.java
@@ -1,10 +1,18 @@
 package com.iws_manager.iws_manager_api.models;
 
-import com.iws_manager.iws_manager_api.models.base.BaseEntity;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.*;
-import lombok.*;
+import com.iws_manager.iws_manager_api.models.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -18,6 +26,6 @@ public class CompanyType extends BaseEntity{
     /**
      * TypeName of the company type (e.g., Public, Private).
      */
-    @Column(name = "name", nullable = false, length = 100)
+    @Column(name = "name", nullable = true, length = 255)
     private String name;
 }
