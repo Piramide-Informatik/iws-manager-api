b2a6e037d36420a04c6ecff20fe36df06b44c208
svaleria <svaleria.gonzales@gmail.com>
2025-07-01 00:10:34 -0400

    employee repository done

---
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.iws_manager.iws_manager_api.repositories;
+
+import com.iws_manager.iws_manager_api.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByLastname(String lastname);
+    Employee findByEmail(String email);
+    List<Employee> findByTitleId(Long titleId);
+    List<Employee> findBySalutationId(Long salutationId);
+    List<Employee> findByQualificationFZId(Long qualificationFZId);
+    List<Employee> findByCustomerId(Long customerId);
+}
