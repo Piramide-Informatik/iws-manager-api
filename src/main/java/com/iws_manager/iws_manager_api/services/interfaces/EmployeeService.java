309bfd55ac5ec8c1045dd7e534c2bffaffd7c9d8
svaleria <svaleria.gonzales@gmail.com>
2025-07-01 00:24:37 -0400

    interface created

---
+++ src/main/java/com/iws_manager/iws_manager_api/services/interfaces/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.iws_manager.iws_manager_api.services.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.iws_manager.iws_manager_api.models.Branch;
+
+public interface BranchService {
+
+    Employee create(Employee employee);
+    Optional<Employee> findById(Long id);
+    List<Employee> findAll();
+    Employee update(Long id, Employee employee);
+    void delete(Long id);
+
+    List<Employee> findByLastname(String lastname);
+    Optional<Employee> findByEmail(String email);
+    List<Employee> findByTitleId(Long titleId);
+    List<Employee> findBySalutationId(Long salutationId);
+    List<Employee> findByQualificationFZId(Long qualificationFZId);
+    List<Employee> findByCustomerId(Long customerId);
+}
