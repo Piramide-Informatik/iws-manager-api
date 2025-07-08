efb4b621642828ebe03df3d32e03aaaa5a23db74
svaleria <svaleria.gonzales@gmail.com>
2025-07-08 17:24:10 -0400

    states ordered by name

--- src/main/java/com/iws_manager/iws_manager_api/repositories/StateRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/StateRepository.java
@@ -3,8 +3,9 @@ package com.iws_manager.iws_manager_api.repositories;
 import com.iws_manager.iws_manager_api.models.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 
 @Repository
 public interface StateRepository extends JpaRepository<State, Long> {
+    List<State> findAllByOrderByNameAsc();
 }
