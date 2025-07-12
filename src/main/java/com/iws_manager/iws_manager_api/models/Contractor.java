d3e86c7bcdfa789c70a7daff8424261657b2cf82
svaleria <svaleria.gonzales@gmail.com>
2025-07-12 13:19:45 -0400

    contractor model implemented

---
+++ src/main/java/com/iws_manager/iws_manager_api/models/Contractor.java
@@ -0,0 +1,53 @@
+package com.iws_manager.iws_manager_api.models;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.iws_manager.iws_manager_api.models.base.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "contractor")
+@AttributeOverride(name = "id", column = @Column(name = "contractorid"))
+public class Contractor extends BaseEntity{
+
+    @Column(name = "city", length = 255)
+    private String city;
+
+    @Column(name = "contractorlabel", length = 255)
+    private String label;
+    
+    @Column(name = "contractorname", length = 255)
+    private String name;
+
+    @Column(name = "contractorno", length = 255)
+    private String number;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "countryid", referencedColumnName = "countryid")
+    private Country country;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customerid", referencedColumnName = "customerid")
+    private Customer customer;
+
+    @Column(name = "street", length = 255)
+    private String street;
+
+    @Column(name = "taxno", length = 255)
+    private String taxNumber;
+
+    @Column(name = "zipcode", length = 255)
+    private String zipCode;
+
+    /*implement subcontracts for the next functionality */
+    // @OneToMany(mappedBy = "contractor", cascade = CascadeType.ALL, orphanRemoval = true)
+    // @ToString.Exclude
+    // private List<Subcontract> subcontracts = new ArrayList<>();
+}
