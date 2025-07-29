087a16d8888041fe092b1d2bc6554e2194eea2f7
svaleria <svaleria.gonzales@gmail.com>
2025-07-29 18:51:47 -0400

    added findby functions

--- src/main/java/com/iws_manager/iws_manager_api/repositories/SubcontractYearRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/SubcontractYearRepository.java
@@ -1,6 +1,8 @@
 package com.iws_manager.iws_manager_api.repositories;
 
+import java.time.LocalDate;
 import java.util.List;
+
 import com.iws_manager.iws_manager_api.models.SubcontractYear;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubcontractYearRepository extends JpaRepository<SubcontractYear, Long> {
+    List<SubcontractYear> findByMonths(Integer months);
+    List<SubcontractYear> findBySubcontractId(Long subcontractId);
+    List<SubcontractYear> findByYear(LocalDate year);
 }
