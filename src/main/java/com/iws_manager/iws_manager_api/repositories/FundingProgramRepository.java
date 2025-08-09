58f94b57dcca84128d3e1522ec41061135a27ac7
iamfab <fabian2015hd@gmail.com>
2025-08-08 21:37:14 -0400

    feat: funding program repository is implemented

---
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/FundingProgramRepository.java
@@ -0,0 +1,10 @@
+package com.iws_manager.iws_manager_api.repositories;
+
+import com.iws_manager.iws_manager_api.models.FundingProgram;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FundingProgramRepository extends JpaRepository<FundingProgram, Long> {
+}
+
