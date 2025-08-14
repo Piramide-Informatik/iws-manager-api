606b2ba2370c229fb9f4d8ad7e3ee433583c49e3
svaleria <svaleria.gonzales@gmail.com>
2025-08-13 20:19:30 -0400

    repository fixed

--- src/main/java/com/iws_manager/iws_manager_api/repositories/DebtRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/DebtRepository.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.math.BigDecimal;
 
 import com.iws_manager.iws_manager_api.models.Debt;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
