c53396104ace920ec0dce3c2110dedfabc54a792
The-Rigo <rodri.ledezma9483@gmail.com>
2025-08-21 10:51:08 -0400

    add systemfunction service and tests

---
+++ src/main/java/com/iws_manager/iws_manager_api/services/interfaces/SystemFunctionService.java
@@ -0,0 +1,14 @@
+package com.iws_manager.iws_manager_api.services.interfaces;
+
+import com.iws_manager.iws_manager_api.models.SystemFunction;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SystemFunctionService {
+    SystemFunction create(SystemFunction systemFunction);
+    Optional<SystemFunction> findById(Long id);
+    List<SystemFunction> findAll();
+    SystemFunction update(Long id, SystemFunction systemFunctionDetails);
+    void delete(Long id);
+}
