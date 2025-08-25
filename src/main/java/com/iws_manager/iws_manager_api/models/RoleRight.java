da2b9c9f2767486036ef762e36e7e93ea67e3ee7
The-Rigo <rodri.ledezma9483@gmail.com>
2025-08-25 11:24:01 -0400

    implement many-to-one relationship between RoleRight and Role

--- src/main/java/com/iws_manager/iws_manager_api/models/RoleRight.java
+++ src/main/java/com/iws_manager/iws_manager_api/models/RoleRight.java
@@ -1,7 +1,9 @@
 package com.iws_manager.iws_manager_api.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.iws_manager.iws_manager_api.models.base.BaseEntity;
 import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -14,8 +16,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.sound.sampled.AudioFileFormat;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
@@ -23,15 +24,16 @@ import javax.sound.sampled.AudioFileFormat;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@EntityListeners(AudioFileFormat.class)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name="roleright")
-@AttributeOverride(name = "id", column = @Column(name = "rolerightid"))
+@AttributeOverride(name = "id",column = @Column(name = "rolerightid"))
 public class RoleRight extends BaseEntity {
-    @Column(name = "accessright")
+    @Column(name = "accessright", columnDefinition = "INT")
     private Integer accessRight;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "roleid")
+    @JoinColumn(name = "roleid", referencedColumnName = "roleid")
+    @JsonBackReference
     private Role role;
 
 //    @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/iws_manager/iws_manager_api/repositories/RoleRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/RoleRepository.java
@@ -18,4 +18,5 @@ public interface RoleRepository extends JpaRepository<Role,Long> {
     List<Role> findByUserId(@Param("userId") Long userId);
 
     List<Role> findAllByOrderByNameAsc();
+
 }
