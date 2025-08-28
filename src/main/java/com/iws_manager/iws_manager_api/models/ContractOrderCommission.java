4279536524852511c565ba26424a584f04d25077
svaleria <svaleria.gonzales@gmail.com>
2025-08-28 10:49:16 -0400

    model implementation

---
+++ src/main/java/com/iws_manager/iws_manager_api/models/ContractOrderCommission.java
@@ -0,0 +1,35 @@
+package com.iws_manager.iws_manager_api.models;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.iws_manager.iws_manager_api.models.base.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "contractordercommission")
+@AttributeOverride(name = "id", column = @Column(name = "contractcommissionid"))
+public class ContractOrderCommission extends BaseEntity {
+
+    @Column(name = "commission", nullable = true, columnDefinition = "DECIMAL(5,2)")
+    private BigDecimal commission;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "contractid", referencedColumnName = "contractid")
+    private EmploymentContract contract;
+    
+    @Column(name = "fromordervalue", nullable = true, columnDefinition = "DECIMAL(10,2)")
+    private BigDecimal fromOrderValue;
+    
+    @Column(name = "mincommission", nullable = true, columnDefinition = "DECIMAL(10,2)")
+    private BigDecimal minCommission;
+}
