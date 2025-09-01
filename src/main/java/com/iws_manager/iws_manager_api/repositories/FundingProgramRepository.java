8207cca159cb47a00609e3bc224c17cbe8bb21b9
svaleria <svaleria.gonzales@gmail.com>
2025-09-01 12:24:41 -0400

    added function to order asc by name

--- src/main/java/com/iws_manager/iws_manager_api/repositories/FundingProgramRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/FundingProgramRepository.java
@@ -1,10 +1,14 @@
 package com.iws_manager.iws_manager_api.repositories;
 
+import java.util.List;
+
 import com.iws_manager.iws_manager_api.models.FundingProgram;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FundingProgramRepository extends JpaRepository<FundingProgram, Long> {
+    List<FundingProgram> findAllByOrderByNameAsc();
 }
 
