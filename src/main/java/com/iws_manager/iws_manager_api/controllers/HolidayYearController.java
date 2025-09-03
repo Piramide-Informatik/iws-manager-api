0632f381bc3cc5448ecfa8cc8c7f324c32b95c9b
The-Rigo <rodri.ledezma9483@gmail.com>
2025-09-03 12:31:40 -0400

    delete holidayyear

--- src/main/java/com/iws_manager/iws_manager_api/controllers/HolidayYearController.java
+++ src/main/java/com/iws_manager/iws_manager_api/controllers/HolidayYearController.java
@@ -5,6 +5,7 @@ import com.iws_manager.iws_manager_api.services.interfaces.HolidayYearService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,12 @@ public class HolidayYearController {
     @ResponseStatus(HttpStatus.CREATED)
     public HolidayYear createNextYear(@PathVariable Long id) {
         return holidayYearService.createNextYear(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> deleteHolidayYear(@PathVariable Long id) {
+        holidayYearService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
