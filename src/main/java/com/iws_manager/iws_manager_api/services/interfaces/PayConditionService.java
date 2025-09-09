bf8e39c367aa400e5c902b041ff91ebea860704c
svaleria <svaleria.gonzales@gmail.com>
2025-09-09 10:18:42 -0400

    service interface

---
+++ src/main/java/com/iws_manager/iws_manager_api/services/interfaces/PayConditionService.java
@@ -0,0 +1,19 @@
+package com.iws_manager.iws_manager_api.services.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.iws_manager.iws_manager_api.models.PayCondition;
+
+public interface PayConditionService {
+    PayCondition create(PayCondition payCondition);
+    Optional<PayCondition> findById(Long id);
+    List<PayCondition> findAll();
+    PayCondition update(Long id, PayCondition payConditionDetails);
+    void delete(Long id);
+
+    List<PayCondition> getAllByOrderByNameAsc();
+    
+    List<PayCondition> getByName(String name);
+    List<PayCondition> getByDeadline(Integer deadline);
+}
