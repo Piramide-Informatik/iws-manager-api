462415ff4875044ea666d790db3b4947e7a831b8
The-Rigo <rodri.ledezma9483@gmail.com>
2025-10-16 11:59:05 -0400

    order by name Project Status

--- src/main/java/com/iws_manager/iws_manager_api/repositories/ProjectStatusRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/ProjectStatusRepository.java
@@ -4,7 +4,10 @@ import com.iws_manager.iws_manager_api.models.ProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, Long> {
+    List<ProjectStatus> findAllByOrderByNameAsc();
 }
