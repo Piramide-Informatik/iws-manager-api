0ddd85e8e0cfbc29b7a149c4bbad3c4e0af385fc
svaleria <svaleria.gonzales@gmail.com>
2025-10-21 12:27:11 -0400

    feat: subcontractyear optimization

--- src/main/java/com/iws_manager/iws_manager_api/repositories/SubcontractYearRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/SubcontractYearRepository.java
@@ -4,16 +4,24 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.iws_manager.iws_manager_api.models.SubcontractYear;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubcontractYearRepository extends JpaRepository<SubcontractYear, Long> {
-    List<SubcontractYear> findByMonths(Integer months);
-    List<SubcontractYear> findBySubcontractId(Long subcontractId);
-    List<SubcontractYear> findByYear(LocalDate year);
 
+    @EntityGraph(attributePaths = {"subcontract"})
+    List<SubcontractYear> findByMonths(Integer months);
+
+    @EntityGraph(attributePaths = {"subcontract"})
+    List<SubcontractYear> findBySubcontractId(Long subcontractId);
+
+    @EntityGraph(attributePaths = {"subcontract"})
+    List<SubcontractYear> findByYear(LocalDate year);
+    
+    @EntityGraph(attributePaths = {"subcontract"})
     List<SubcontractYear> findBySubcontractIdOrderByYearAsc(Long subcontractId);
 
 }
