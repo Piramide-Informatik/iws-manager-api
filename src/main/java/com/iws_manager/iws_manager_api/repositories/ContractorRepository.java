6a37a69cf7cd7273192f8ae33f98a40d59d68295
The-Rigo <rodri.ledezma9483@gmail.com>
2025-10-23 12:24:27 -0400

    feat: contractor optimization

--- src/main/java/com/iws_manager/iws_manager_api/repositories/ContractorRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/ContractorRepository.java
@@ -1,16 +1,30 @@
 package com.iws_manager.iws_manager_api.repositories;
 
+import com.iws_manager.iws_manager_api.models.ContactPerson;
 import com.iws_manager.iws_manager_api.models.Contractor;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ContractorRepository extends JpaRepository<Contractor,Long> {
+    @EntityGraph(attributePaths = {"customer", "country"})
+    Optional<Contractor> findById(Long id);
+    @EntityGraph(attributePaths = {"customer", "country"})
     List<Contractor> findAllByOrderByNameAsc();
+
+    @EntityGraph(attributePaths = {"customer", "country"})
     List<Contractor> findByCustomerId(Long customerId);
+
+    @EntityGraph(attributePaths = {"customer", "country"})
     List<Contractor> findByCountryId(Long countryId);
 
+    @EntityGraph(attributePaths = {"customer", "country"})
     List<Contractor> findByCustomerIdOrderByLabelAsc(Long customerId);
+
+    @EntityGraph(attributePaths = {"customer", "country"})
     List<Contractor> findByCustomerIdOrderByNameAsc(Long customerId);
 }
