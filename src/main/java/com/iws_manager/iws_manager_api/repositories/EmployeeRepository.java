6cb47c8df2584147ff08cc316d6d6bff2ea68219
The-Rigo <rodri.ledezma9483@gmail.com>
2025-10-24 08:45:53 -0400

    feat: employee optimization

--- src/main/java/com/iws_manager/iws_manager_api/repositories/EmployeeRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.iws_manager.iws_manager_api.repositories;
 
 import com.iws_manager.iws_manager_api.models.Employee;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,13 +9,20 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findAllByOrderByEmployeenoAsc();
-    
+
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findByLastname(String lastname);
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     Employee findByEmail(String email);
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findByTitleId(Long titleId);
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findBySalutationId(Long salutationId);
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findByQualificationFZId(Long qualificationFZId);
-    //find by customer Id 
+    //find by customer Id
+    @EntityGraph(attributePaths = {"qualificationFZ", "salutation", "title"})
     List<Employee> findByCustomerIdOrderByEmployeenoAsc(Long customerId);
 }
