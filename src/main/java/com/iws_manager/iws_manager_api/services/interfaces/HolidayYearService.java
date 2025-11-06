b36257a4eb747de8d84560e49a050d9d4ca62f39
svaleria <svaleria.gonzales@gmail.com>
2025-11-06 19:08:49 -0400

    service updated

--- src/main/java/com/iws_manager/iws_manager_api/services/interfaces/HolidayYearService.java
+++ src/main/java/com/iws_manager/iws_manager_api/services/interfaces/HolidayYearService.java
@@ -1,13 +1,21 @@
 package com.iws_manager.iws_manager_api.services.interfaces;
 
+import java.time.LocalDate;
+
 import com.iws_manager.iws_manager_api.models.HolidayYear;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HolidayYearService {
+    HolidayYear create(HolidayYear holidayYear);
+    Optional<HolidayYear> findById(Long id);
     List<HolidayYear> getAll();
-    List<HolidayYear> getByPublicHolidayId(Long publicHolidayId);
-    HolidayYear createNextYear(Long publicHolidayId);
+    HolidayYear update(Long id, HolidayYear holidayYearDetails);
     void delete(Long id);
 
+    List<HolidayYear> getByPublicHolidayId(Long publicHolidayId);
+    HolidayYear createNextYear(Long publicHolidayId);
+    boolean existsByYearAndPublicHoliday(LocalDate year, Long publicHolidayId);
+    List<HolidayYear> getByYear(LocalDate year);    
 }
