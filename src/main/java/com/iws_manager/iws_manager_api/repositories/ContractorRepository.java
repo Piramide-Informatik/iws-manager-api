f41f41e5b830f5d4571f490ad4e9f1f88f60e3a9
svaleria <svaleria.gonzales@gmail.com>
2025-11-12 11:43:07 -0400

    added methods to validate label attribute

--- src/main/java/com/iws_manager/iws_manager_api/repositories/ContractorRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/ContractorRepository.java
@@ -2,12 +2,16 @@ package com.iws_manager.iws_manager_api.repositories;
 
 import com.iws_manager.iws_manager_api.models.ContactPerson;
 import com.iws_manager.iws_manager_api.models.Contractor;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ContractorRepository extends JpaRepository<Contractor,Long> {
@@ -27,4 +31,14 @@ public interface ContractorRepository extends JpaRepository<Contractor,Long> {
 
     @EntityGraph(attributePaths = {"customer", "customer.branch", "customer.companytype", "customer.country", "customer.state","country"})
     List<Contractor> findByCustomerIdOrderByNameAsc(Long customerId);
+
+    // VALIDATIONS
+    // Label must be unique
+    // CREATION - verify if label exists for specific customer (case-insensitive)
+    @Query("SELECT COUNT(c) > 0 FROM Contractor c WHERE UPPER(c.label) = UPPER(:label) AND c.customer.id = :customerId")
+    boolean existsByLabelIgnoreCaseAndCustomerId(@Param("label") String label, @Param("customerId") Long customerId);
+    
+    // UPDATING - verify if label exists for specific customer with exception of the same item (case-insensitive)
+    @Query("SELECT COUNT(c) > 0 FROM Contractor c WHERE UPPER(c.label) = UPPER(:label) AND c.customer.id = :customerId AND c.id != :excludeId")
+    boolean existsByLabelIgnoreCaseAndCustomerIdAndIdNot(@Param("label") String label, @Param("customerId") Long customerId, @Param("excludeId") Long excludeId);
 }
