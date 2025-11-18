0878a85e9e77dcc8fabe6a14784942e610337cd6
svaleria <svaleria.gonzales@gmail.com>
2025-11-18 12:18:06 -0400

    modified contract order commission to consumed shared resources

--- src/main/java/com/iws_manager/iws_manager_api/dtos/contractordercommission/ContractOrderCommissionInputDTO.java
+++ src/main/java/com/iws_manager/iws_manager_api/dtos/contractordercommission/ContractOrderCommissionInputDTO.java
@@ -1,10 +1,11 @@
 package com.iws_manager.iws_manager_api.dtos.contractordercommission;
 
+import com.iws_manager.iws_manager_api.dtos.shared.BasicReferenceDTO;
 import java.math.BigDecimal;
 
 public record ContractOrderCommissionInputDTO(
     BigDecimal commission,
     BigDecimal fromOrderValue, 
     BigDecimal minCommission,
-    BasicContractReferenceDTO basicContract
+    BasicReferenceDTO basicContract  
 ) {}
