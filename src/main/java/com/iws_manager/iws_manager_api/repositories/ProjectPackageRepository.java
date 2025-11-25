3903c1f55fae4ef5026232bfe301a68c2499fd4a
svaleria <svaleria.gonzales@gmail.com>
2025-11-25 09:55:39 -0400

    added spaces

--- src/main/java/com/iws_manager/iws_manager_api/repositories/ProjectPackageRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/ProjectPackageRepository.java
@@ -1,16 +1,28 @@
 package com.iws_manager.iws_manager_api.repositories;
 
 import com.iws_manager.iws_manager_api.models.ProjectPackage;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+
 @Repository
 public interface ProjectPackageRepository extends JpaRepository<ProjectPackage, Long> {
+    @EntityGraph(attributePaths = {"project"})
     List<ProjectPackage> findAllByOrderByPackageTitleAsc();
+
+    @EntityGraph(attributePaths = {"project"})
     List<ProjectPackage> findAllByOrderByPackageSerialAsc();
+
+    @EntityGraph(attributePaths = {"project"})
     List<ProjectPackage> findAllByOrderByPackageNoAsc();
+
+    @EntityGraph(attributePaths = {"project"})
     List<ProjectPackage> findAllByOrderByStartDate();
+
+    @EntityGraph(attributePaths = {"project"})
     List<ProjectPackage> findAllByOrderByEndDate();
 }
