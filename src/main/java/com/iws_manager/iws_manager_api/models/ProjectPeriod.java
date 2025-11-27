b7975f53f1356e5f84375afc7027095e857b41b3
The-Rigo <rodri.ledezma9483@gmail.com>
2025-11-27 16:22:30 -0400

    feat: add projectPeriod model and test

---
+++ src/main/java/com/iws_manager/iws_manager_api/models/ProjectPeriod.java
@@ -0,0 +1,43 @@
+package com.iws_manager.iws_manager_api.models;
+
+import com.iws_manager.iws_manager_api.models.base.BaseEntity;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "projectperiod")
+@AttributeOverride(name ="id", column = @Column(name = "projectperiodid"))
+public class ProjectPeriod extends BaseEntity {
+    @Column(name="packageno", length = 255)
+    private Integer periodNo;
+
+    @Column(name = "start", columnDefinition = "DATE")
+    private LocalDate startDate;
+
+    @Column(name = "end", columnDefinition = "DATE")
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "projectid")
+    private Project project;
+}
