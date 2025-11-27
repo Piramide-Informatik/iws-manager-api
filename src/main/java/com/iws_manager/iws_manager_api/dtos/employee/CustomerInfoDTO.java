891df66b87a17705ce06853ac93286d58bb0cef7
svaleria <svaleria.gonzales@gmail.com>
2025-11-27 17:41:29 -0400

    moved customerInfoDto to shared dtos folder

--- src/main/java/com/iws_manager/iws_manager_api/dtos/employee/CustomerInfoDTO.java
+++
@@ -1,8 +0,0 @@
-package com.iws_manager.iws_manager_api.dtos.employee;
-
-public record CustomerInfoDTO(
-    Long id,
-    String customername1,
-    String customername2,
-    String city
-) {}
--- src/main/java/com/iws_manager/iws_manager_api/dtos/employee/EmployeeDetailDTO.java
+++ src/main/java/com/iws_manager/iws_manager_api/dtos/employee/EmployeeDetailDTO.java
@@ -1,25 +1,26 @@
 package com.iws_manager.iws_manager_api.dtos.employee;
 
+import com.iws_manager.iws_manager_api.dtos.shared.CustomerInfoDTO;
 import java.time.LocalDate;
 
 public record EmployeeDetailDTO(
-    Long id,
-    String firstname,
-    String lastname,
-    String email,
-    Integer employeeno,
-    String label,
-    String phone,
-    LocalDate coentrepreneursince,
-    LocalDate generalmanagersince,
-    LocalDate shareholdersince,
-    LocalDate soleproprietorsince,
-    String qualificationkmui,
-    
-    // Relaciones optimizadas
-    CustomerInfoDTO customer,
-    QualificationFZInfoDTO qualificationFZ,
-    SalutationInfoDTO salutation,
-    TitleInfoDTO title,
-    EmployeeCategoryInfoDTO employeeCategory
-) {}
+        Long id,
+        String firstname,
+        String lastname,
+        String email,
+        Integer employeeno,
+        String label,
+        String phone,
+        LocalDate coentrepreneursince,
+        LocalDate generalmanagersince,
+        LocalDate shareholdersince,
+        LocalDate soleproprietorsince,
+        String qualificationkmui,
+
+        // Relaciones optimizadas
+        CustomerInfoDTO customer,
+        QualificationFZInfoDTO qualificationFZ,
+        SalutationInfoDTO salutation,
+        TitleInfoDTO title,
+        EmployeeCategoryInfoDTO employeeCategory) {
+}
