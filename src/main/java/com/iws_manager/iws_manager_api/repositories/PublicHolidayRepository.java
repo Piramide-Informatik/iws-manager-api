7a2c82097579836915c9f71637ba10c55dafed44
svaleria <svaleria.gonzales@gmail.com>
2025-12-11 11:41:14 -0400

    repository updated

--- src/main/java/com/iws_manager/iws_manager_api/repositories/PublicHolidayRepository.java
+++ src/main/java/com/iws_manager/iws_manager_api/repositories/PublicHolidayRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.time.LocalDate;
 
 @Repository
 public interface PublicHolidayRepository extends JpaRepository<PublicHoliday, Long> {
@@ -22,4 +23,12 @@ public interface PublicHolidayRepository extends JpaRepository<PublicHoliday, Lo
     @EntityGraph(attributePaths = {"stateHolidays", "holidayYears"})
     @Query("SELECT COALESCE(MAX(p.sequenceNo), 0) FROM PublicHoliday p")
     Long findMaxSequenceNo();
+
+    // Method to get holidays between dates
+    @Query("SELECT p FROM PublicHoliday p WHERE p.date BETWEEN :startDate AND :endDate")
+    List<PublicHoliday> findHolidaysBetweenDates(LocalDate startDate, LocalDate endDate);
+    
+    // Method to get holidays by year
+    @Query("SELECT p FROM PublicHoliday p WHERE YEAR(p.date) = :year")
+    List<PublicHoliday> findHolidaysByYear(Integer year);
 }
