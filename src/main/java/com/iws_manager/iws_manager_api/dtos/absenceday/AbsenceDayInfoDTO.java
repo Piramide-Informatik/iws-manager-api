7ab7ad10deebb103092d4c3cc90ddb94a1c62cd9
svaleria <svaleria.gonzales@gmail.com>
2025-12-16 16:20:21 -0400

    updated info dto

--- src/main/java/com/iws_manager/iws_manager_api/dtos/absenceday/AbsenceDayInfoDTO.java
+++ src/main/java/com/iws_manager/iws_manager_api/dtos/absenceday/AbsenceDayInfoDTO.java
@@ -1,14 +1,15 @@
 package com.iws_manager.iws_manager_api.dtos.absenceday;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.iws_manager.iws_manager_api.dtos.shared.BasicReferenceDTO;
+import com.iws_manager.iws_manager_api.dtos.shared.AbsenceTypeInfoDTO;
+import com.iws_manager.iws_manager_api.dtos.shared.EmployeeBasicDTO;
 import java.time.LocalDate;
 
 public record AbsenceDayInfoDTO(
     Long id,
     @JsonFormat(pattern = "yyyy-MM-dd")
     LocalDate absenceDate,
-    BasicReferenceDTO absenceType,
-    BasicReferenceDTO employee,
+    AbsenceTypeInfoDTO absenceType,
+    EmployeeBasicDTO employee,
     Integer version
 ) {}
