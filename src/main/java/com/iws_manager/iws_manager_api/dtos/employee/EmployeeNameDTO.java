a275eb4c29a5b4009eb41114758d901e144333d7
svaleria <svaleria.gonzales@gmail.com>
2026-01-21 11:53:01 -0400

    bug: employee - get fullname should show employeeno

--- src/main/java/com/iws_manager/iws_manager_api/dtos/employee/EmployeeNameDTO.java
+++ src/main/java/com/iws_manager/iws_manager_api/dtos/employee/EmployeeNameDTO.java
@@ -1,8 +1,9 @@
 package com.iws_manager.iws_manager_api.dtos.employee;
 
 public record EmployeeNameDTO(
-        Long id,
-        String firstname,
-        String lastname,
-        String fullname) {
+                Long id,
+                Integer employeeno,
+                String firstname,
+                String lastname,
+                String fullname) {
 }
--- src/main/java/com/iws_manager/iws_manager_api/mappers/EmployeeMapper.java
+++ src/main/java/com/iws_manager/iws_manager_api/mappers/EmployeeMapper.java
@@ -239,6 +239,7 @@ public class EmployeeMapper {
 
         return new EmployeeNameDTO(
                 entity.getId(),
+                entity.getEmployeeno(),
                 entity.getFirstname(),
                 entity.getLastname(),
                 buildFullName(entity.getFirstname(), entity.getLastname()));
